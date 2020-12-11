38af038ff9259d8aa01477227b9e4e87ab6eedd8
Eskendarov <envereskendarov@gmail.com>
2020-12-11 08:10:11 +0300

    0.1. FileOutputStream. [#389228]

--- src/main/java/ru/job4j/array/Matrix.java
+++ src/main/java/ru/job4j/array/Matrix.java
@@ -1,12 +1,23 @@
 package ru.job4j.array;
 
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
 public class Matrix {
+
     public int[][] multiple(int size) {
         int[][] table = new int[size][size];
+        final StringBuilder builder = new StringBuilder();
         for (int i = 0; i < table.length; i++) {
             for (int j = 0; j < table.length; j++) {
-                table[i][j] = (i + 1) * (j + 1);
+                builder.append(table[i][j] = (i + 1) * (j + 1));
             }
+            builder.append(System.lineSeparator());
+        }
+        try (OutputStream out = new FileOutputStream("result.txt")) {
+            out.write(builder.toString().getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return table;
     }
