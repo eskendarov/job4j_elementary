a7f932f1989058d722e63cc4e451a44da2d60b9a
enver <envereskendarov@gmail.com>
2021-05-24 16:57:08 +0300

    4.1. Аргументы [#295521 #159456]

--- src/main/java/ru/job4j/calculator/ArgMethod.java
+++ src/main/java/ru/job4j/calculator/ArgMethod.java
@@ -6,20 +6,13 @@ package ru.job4j.calculator;
  */
 public class ArgMethod {
 
-    public static void hello(Object... args) {
-        System.out.print("Hello, ");
-        for (Object obj : args) {
-            System.out.print(obj + " ");
-        }
-        System.out.println();
+    public static void hello(String name, int age) {
+        System.out.printf("Hello, %s, %d", name, age);
     }
 
     public static void main(String[] args) {
         final String name = "Petr Arsentev";
         final int age = 33;
-        ArgMethod.hello();
         ArgMethod.hello(name, age);
-        ArgMethod.hello(age);
-        ArgMethod.hello(name, name, name);
     }
 }
