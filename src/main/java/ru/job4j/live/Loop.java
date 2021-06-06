75efe441c6bd595e4d143b7a4e4453622aefe76b
enver <envereskendarov@gmail.com>
2021-06-06 05:07:57 +0300

    5.9.1. Генерация конструкций в IDEA [#279134]

---
+++ src/main/java/ru/job4j/live/Loop.java
@@ -0,0 +1,16 @@
+package ru.job4j.live;
+
+/**
+ * Loop Class description goes here.
+ *
+ * @author Enver Eskendarov
+ * @version 1.0 06/06/2021
+ */
+public class Loop {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
