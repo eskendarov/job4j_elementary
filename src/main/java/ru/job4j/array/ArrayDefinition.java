891fe8f49be98de7830cc82273046587ca4211de
enver <envereskendarov@gmail.com>
2021-06-06 05:15:34 +0300

    6.0.3.1. Массивы и цикл for-each [#333581]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -11,10 +11,9 @@ public class ArrayDefinition {
         names[1] = "Dima";
         names[2] = "Serega";
         names[3] = "Valera";
-        System.out.println(names[0]);
-        System.out.println(names[1]);
-        System.out.println(names[2]);
-        System.out.println(names[3]);
+        for (String name : names) {
+            System.out.println(name);
+        }
         System.out.println("Размер массива 'ages' равен: " + ages.length);
         System.out.println("Размер массива 'surnames' равен: " + surnames.length);
         System.out.println("Размер массива 'prices' равен: " + prices.length);
